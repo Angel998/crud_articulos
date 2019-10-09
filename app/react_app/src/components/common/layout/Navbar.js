7a4ca247645c32e1ad216f33412d00d13cc68006
Angel Hernandez <angel_hernandez998@hotmail.com>
2019-10-09 11:12:14 -0600

    Fix: Correccion al Navbar en telefonos y tablets

--- app/react_app/src/components/common/layout/Navbar.js
+++ app/react_app/src/components/common/layout/Navbar.js
@@ -11,28 +11,50 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="blue darken-1">
-        <div className="container">
-          <div className="nav-wrapper">
-            <a href="#!" className="brand-logo">
-              A2H
-            </a>
-            <ul id="nav-mobile" className="right">
-              <li>
-                <Link to="/articulos">Articulos</Link>
-              </li>
-              <li>
-                <Link to="/nuevo_articulo">Nuevo</Link>
-              </li>
-              <li>
-                <a href="#!" onClick={this.onLogOut}>
-                  <i className="material-icons">exit_to_app</i>
-                </a>
-              </li>
-            </ul>
+      <React.Fragment>
+        <nav className="blue darken-1">
+          <div className="container">
+            <div className="nav-wrapper">
+              <a href="#!" className="brand-logo">
+                A2H
+              </a>
+              <a
+                href="#!"
+                data-target="mobile-demo"
+                className="sidenav-trigger"
+              >
+                <i className="material-icons">menu</i>
+              </a>
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li>
+                  <Link to="/articulos">Articulos</Link>
+                </li>
+                <li>
+                  <Link to="/nuevo_articulo">Nuevo</Link>
+                </li>
+                <li>
+                  <a href="#!" onClick={this.onLogOut}>
+                    <i className="material-icons">exit_to_app</i>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        <ul className="sidenav" id="mobile-demo">
+          <li>
+            <Link to="/articulos">Articulos</Link>
+          </li>
+          <li>
+            <Link to="/nuevo_articulo">Nuevo</Link>
+          </li>
+          <li>
+            <a href="#!" onClick={this.onLogOut}>
+              <i className="material-icons">exit_to_app</i>
+            </a>
+          </li>
+        </ul>
+      </React.Fragment>
     );
   }
 }
